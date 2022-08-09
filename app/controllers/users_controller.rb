13f628f8d62e605596066926aa43cc16165a9f76
Sidd <dandekarsiddhesh.02@gmail.com>
2022-08-10 01:03:54 +0530

    Added skip_before_action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  skip_before_action :verify_authenticity_token
 
   def index
     render plain: User.all.map {|user| user.to_pleasant_string}.join("\n")
