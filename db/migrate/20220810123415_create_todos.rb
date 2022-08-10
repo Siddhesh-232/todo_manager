86b0f6e533412ea75eb55f0cb2a20b4021cd3400
Sidd <dandekarsiddhesh.02@gmail.com>
2022-08-10 18:06:50 +0530

    Update database migrations

---
+++ db/migrate/20220810123415_create_todos.rb
@@ -0,0 +1,10 @@
+class CreateTodos < ActiveRecord::Migration[7.0]
+  def change
+    create_table :todos do |t|
+      t.text :todo_text
+      t.date :due_date
+
+      t.timestamps
+    end
+  end
+end
