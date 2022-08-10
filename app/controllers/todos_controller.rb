65af6fbcf4ab4b6aa5a03f33053955ac1fb0b591
Sidd <dandekarsiddhesh.02@gmail.com>
2022-08-10 16:55:11 +0530

    Revert "ERB Templating & Asset Pipeline"
    This reverts commit d96c6b445e316e85ae7fd0a312cb5a6ef8818270.

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,15 +3,13 @@ class TodosController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
-    #render plain: Todo.order(:due_date).map {|todo| todo.to_pleasant_string}.join("\n")
-    render "index"
+    render plain: Todo.order(:due_date).map {|todo| todo.to_pleasant_string}.join("\n")
   end
 
   def show
     id = params[:id]
     todo = Todo.find(id)
-    #render plain: todo.to_pleasant_string
-    render "todo"
+    render plain: todo.to_pleasant_string
   end
 
   def create
