246c3837c2fb0b8a30d054ff00cb63052355f4b8
Sidd <dandekarsiddhesh.02@gmail.com>
2022-08-10 17:12:22 +0530

    Revert "Revert "ERB Templating & Asset Pipeline""
    This reverts commit 65af6fbcf4ab4b6aa5a03f33053955ac1fb0b591.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     # get "todos", to: "todos#index"
     # post "todos", to: "todos#create"
     # get "todos/:id", to: "todos#show"
-
+    get "/" => "home#index"
     resources :users
     post "users", to: "users#login"
     resources :todos
